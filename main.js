c6e16efc38e2705f06b516d4f8207cf3a832c27a
Turvenuija <132452863+turvenuija1@users.noreply.github.com>
2023-09-11 17:10:32 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function discord() {
+    const name = "trvnj";
+    navigator.clipboard.writeText(name);
+    document.getElementById("success").innerText = "Name copied to clipboard.";
+}
+
+function github() {
+    window.location.href = "https://github.com/turvenuija1";
+}
+
+function steam() {
+    window.location.href = "https://steamcommunity.com/id/musta_neekeri/";
+}
