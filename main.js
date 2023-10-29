b3315fd2fb994ade57a5751a9740cc3b4877fec6
æ <132452863+turvenuija1@users.noreply.github.com>
2023-10-29 13:49:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ function discord() {
     document.getElementById("success").innerText = "Name copied to clipboard.";
 }
 
-function github() {
-    window.location.href = "https://github.com/turvenuija1";
-}
-
 function steam() {
     window.location.href = "https://steamcommunity.com/id/trvnj/";
 }
